4f4f9439148de40766751adc22b122c66ed43116
gilairmay <gilairmay@gmail.com>
2020-08-05 08:48:37 -0300

    Revert "Initial commit"
    This reverts commit 2736c3d33c16e3e55fe24a2c463529295c1b390f.

--- src/components/Card/Icon/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Icon(props) {
-    return (
-        <div className={`icon fa ${props.children} fa-thumbs-o-up card__descriptionIcon`} />
-    )
-}
--- src/components/Card/Price/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Price(props) {
-    return (
-        <div className="card__price">{props.children}</div>
-    )
-}
